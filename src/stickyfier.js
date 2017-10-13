5a055b1361edd1b4ddd3c07c957dd6dfbab1e046
Chreznor <chechur@bk.ru>
2017-10-13 18:59:38 +0300

    added a throttle function to the scroll event listener

--- src/stickyfier.js
+++ src/stickyfier.js
@@ -22,13 +22,26 @@ const stickyfier = (HeadlineContainers, listContainers) => {
       }
   }
 
+  let handler;
+
+  function throttle(func, wait) {
+    let time = Date.now();
+    return function() {
+      if ((time + wait - Date.now()) < 0) {
+        func();
+        time = Date.now();
+      }
+    }
+  }
+
   //returning two functions that add or remove the event listener to the window scroll
   return {
       initialize() {
-          window.addEventListener('scroll', fixedHeadline);
+          handler = throttle(fixedHeadline, 100);
+          window.addEventListener('scroll', handler);
       },
       disable() {
-          window.removeEventListener('scroll', fixedHeadline);
+          window.removeEventListener('scroll', handler);
           //once the event lisntener is removed, the attached classes have to go too
           HeadlineContainers.forEach(element => {
               element.classList.remove('fixed-nav');
